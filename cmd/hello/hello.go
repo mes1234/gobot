1e2371470a83145bc6f3d2b64ad20b9ecbfbe63c
FWitold Kepczynski <witold.kepczynski@gmail.com>
2021-05-16 23:29:02 +0200

    added simple ping

--- cmd/hello/hello.go
+++ cmd/hello/hello.go
@@ -1,10 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/akamensky/argparse"
+	"github.com/nats-io/nats.go"
+)
 
 func main() {
-	for {
-		fmt.Println("Hello")
+
+	parser := argparse.NewParser("flags", "Ping Pong tool for NATS connectivity")
+	subscriberFlag := parser.Flag("v", "verbose", &argparse.Options{Help: "Enable subcriber mode"})
+	serverAddr := parser.String("s", "server", &argparse.Options{Required: true, Help: "server address is requried"})
+
+	// Parse input
+	err := parser.Parse(os.Args)
+	if err != nil {
+		// In case of error print error and print usage
+		// This can also be done by passing -h or --help flags
+		fmt.Print(parser.Usage(err))
+		os.Exit(1)
 	}
 
+	// Connect to a server
+	nc, _ := nats.Connect(*serverAddr)
+	defer nc.Drain()
+	defer nc.Close()
+
+	if *subscriberFlag {
+		// Simple Async Subscriber
+		fmt.Printf("Start reciving")
+		nc.Subscribe("pingpong", func(m *nats.Msg) {
+			fmt.Printf("Received a message: %s\n", string(m.Data))
+		})
+		sleep()
+
+	} else {
+		rand.Seed(time.Now().UnixNano())
+		seed := rand.Intn(300)
+		for i := 0; i < 10; i++ {
+			msg := fmt.Sprintf("Sent a message: %s, seed %d, : count %d/10\n", "Yello", seed, i+1)
+			fmt.Print(msg)
+			nc.Publish("pingpong", []byte(msg))
+		}
+	}
+
+}
+
+func sleep() {
+	for {
+		time.Sleep(1000)
+	}
 }
